9ceb8dcc9c24ea535d6c84f65e55bf2ba494f494
mrunal <mrunalbhoir100@gmail.com>
2022-07-25 22:38:48 +0530

    UC5 Creating multiple contacts

--- src/com/addresssystem/Main.java
+++ src/com/addresssystem/Main.java
@@ -19,27 +19,7 @@ public class Main {
         System.out.println("Please enter the details of third contact(firstname,lastname,city,phone number,zipcode)");
         AddressSystem c3= new AddressSystem(s3.nextLine(),s3.nextLine(),s3.nextLine(),s3.nextLong(),s2.nextInt());
 
-          Scanner s=new Scanner(System.in);
-//        System.out.println("enter the contact firstname to edit");
-//        String name=s.next();
 
-        System.out.println("enter the contact firstname whose number need to be deleted ");
-        String name1=s.next();
-        if(name1.equals(c1.firstname))
-        {
-            c1.firstname=  null;
-            System.out.println("Contact deleted");
-        }
-        if(name1.equals(c2.firstname))
-        {
-            c2.firstname= null;
-            System.out.println("Contact deleted");
-        }
-        if(name1.equals(c3.firstname))
-        {
-            c3.firstname= null;
-            System.out.println("Contact deleted");
-        }
 
     }
 
